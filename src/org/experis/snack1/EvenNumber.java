e7d61eee8fa28aaf0435eee2dc79be375e110e08
luca-masera <lucamasera1995@gmail.com>
2024-03-28 14:57:52 +0100

    snack1 result

--- src/org/experis/snack1/EvenNumber.java
+++ src/org/experis/snack1/EvenNumber.java
@@ -1,7 +1,28 @@
 package org.experis.snack1;
 
+import java.util.Scanner;
+
 public class EvenNumber {
     public static void main(String[] args) {
+        //Inizializzo lo scanner
+        Scanner scan = new Scanner(System.in);
 
+        //variabile booleana
+        boolean valid= false;
+
+        //ciclo per chiedere all'utente
+        while(!valid){
+            System.out.println("Inserisci un numero pari fra 10 e 20:");
+            int numberUser = scan.nextInt();;
+            if(numberUser >= 10 && numberUser <=20){
+                valid = true;
+            }
+            if(valid){
+                System.out.println("Ottimo lavoro!");
+            }
+        }
+
+        //Chiudo lo scanner
+        scan.close();
     }
 }
